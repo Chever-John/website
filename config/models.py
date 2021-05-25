11b6e11b6749bdaacbdc9e775e5ea526ccdc8048
ChenweiJiang <mr8god@hotmail.com>
2021-05-26 00:10:25 +0800

    realize blog

--- config/models.py
+++ config/models.py
@@ -16,7 +16,7 @@ class Link(models.Model):
                                          verbose_name="权重",
                                          help_text="权重高展示顺序靠前")
 
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -47,7 +47,7 @@ class SideBar(models.Model):
                                help_text="如果设置的不是HTML类型，可为空")
 
     status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
