349069f8252179d9016d0fa6c4739b238db5bee7
ChenweiJiang <mr8god@hotmail.com>
2021-05-25 22:58:41 +0800

    add three app

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+    profile = os.environ.get('WEBSITE_PROFILE', 'develop')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',"website.settings.%s" % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
